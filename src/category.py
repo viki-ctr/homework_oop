8f755b8f04c4abd130d909a9a214eec24e84eecd
Viktoria Andrushenka <vika.sapriko.9981@gmail.com>
2025-01-13 01:27:18 +0300

    function done

---
+++ src/category.py
@@ -0,0 +1,53 @@
+from src.product import Product
+
+
+class Category:
+    name: str
+    description: str
+    products: list[Product]
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products=None):
+        self.name = name  # Название категории
+        self.description = description  # Описание категории
+        self.products = products if products else []  # Список товаров (объекты Product)
+        Category.category_count += 1  # Количество категорий
+        Category.product_count += len(self.products)  # Количество товаров
+
+    def add_product(self, product: Product):
+        self.products.append(product)
+        Category.product_count += 1
+
+
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    print(category1.name == "Смартфоны")
+    print(category1.description)
+    print(len(category1.products))
+    print(category1.category_count)
+    print(category1.product_count)
+
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category2 = Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product4],
+    )
+
+    print(category2.name)
+    print(category2.description)
+    print(len(category2.products))
+    print(category2.products)
+
+    print(Category.category_count)
+    print(Category.product_count)
---
+++ src/product.py
@@ -0,0 +1,34 @@
+class Product:
+    """Класс для описания продуктов"""
+
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name  # Название товара
+        self.description = description  # Описание товара
+        self.price = price  # Цена товара (с плавающей точкой для копеек)
+        self.quantity = quantity  # Количество в наличии (целое число)
+
+
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(product1.name)
+    print(product1.description)
+    print(product1.price)
+    print(product1.quantity)
+
+    print(product2.name)
+    print(product2.description)
+    print(product2.price)
+    print(product2.quantity)
+
+    print(product3.name)
+    print(product3.description)
+    print(product3.price)
+    print(product3.quantity)
